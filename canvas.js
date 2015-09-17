df6e26b8e233d1a78e58910bf4fa0c7fcc9a6938
Alexander <tishinskiy@mail.ru>
2015-09-17 18:07:18 +0600

    bullets

--- canvas.js
+++ canvas.js
@@ -1,36 +1,67 @@
-var canvas, ctx, w, h, x = 0, y = 0;
+var canvas, ctx, x = 0, y = 0;
 
 var leftR = false;
 var rightR = false;
 var frontM = false;
 var backM = false;
 
+var w = $("#canvas").width();
+var h = $("#canvas").height();
+
+var inc = 0;
+
+var bulet = [];
+
 var plan = {
 	points: [[-10, -10],[10, -10],[10, -2],[30, -2],[30, 2],[10, 2],[10, 10],[-10, 10],],
 	center: [50, 50],
 	angle: 0,
 	bg:"#cc8800",
-	speed: 5,
+	draw:function(){
+		a =  (Math.atan((y - this.center[1]) / (x - this.center[0])));
+		if( (x - this.center[0]) < 0 ) { a = Math.PI + a; }
+		this.angle = a;
+		this.center = baza.center;
+
+		drawObject(this);
+
+	},
 }
+
 var baza = {
 	points:[[15, 15],[15, 10],[10, 10],[20, 0],[10, -10],[15, -10],[15, -15],[-15, -15],[-15, -10],[-10, -10],[-10, 10],[-15, 10],[-15, 15],],
 	center:[50, 50],
 	angle: 0,
 	bg:"#364700",
-	speed: 3
+	speedR: 5,
+	draw:function(){
+		if (leftR == true) {this.angle -= this.speedR * Math.PI / 180;}
+		if (rightR == true) {this.angle += this.speedR * Math.PI / 180;}
+		if (frontM == true) {objectMove(this);}
+		if (backM == true) {objectMove(this, -1);}
+		drawObject(this);
+	},
+	speed: 10,
+
 }
-var bulet = {
-	points: [[0, 0], [5,0]],
+var buletProto = {
+	points: [[0, 0], [8,0]],
 	bg:"#FF3802",
-	center:[],
-	angle: 0,
-	speed: 50,
-	shoot :[]
+	draw:function(){
+		objectMove(this);
+		drawObject(this);
+		buletDell(this);
+	},
+	speed: 20,
+	shoot :[],
+	constructor: function(startP, center, angle) {
+		this.startP = startP;
+		this.center = center;
+		this.angle = angle;
+		return this;
+	}
 }
 
-
-var w = $("#canvas").width();
-var h = $("#canvas").height();
 
 
 
@@ -55,20 +86,10 @@ window.onkeyup = function(e){
 }
 
 $("#canvas").mousedown(function(event) {
-	bc = plan.center;
-	bulet.shoot = [x,y];
-	bulet.angle = plan.angle;
-	bulet.center =[plan.center[0] + 30 * (Math.cos(bulet.angle)), plan.center[1] + 30 * (Math.sin(bulet.angle))];
+	bulet[inc] = Object.create(buletProto).constructor([plan.center[0], plan.center[0]], [plan.center[0] + 30 * (Math.cos(plan.angle)), plan.center[1] + 30 * (Math.sin(plan.angle))], plan.angle);
+	inc++;
+	console.log(bulet);
 });
-
-function drawShoot() {
-	drawObject(bulet);
-	var xpb = bulet.center[0] + bulet.speed * (Math.cos(bulet.angle));
-	var ypb = bulet.center[1] + bulet.speed * (Math.sin(bulet.angle));
-
-	bulet.center[0] =xpb;
-	bulet.center[1] =ypb;
-}
 
 function init() {
 	canvas = document.getElementById("canvas");
@@ -83,6 +104,12 @@ function angleValue(obj) {
 	obj.angle = a;
 }
 
+function buletDell(obj) {
+	if(Math.sqrt(Math.pow(Math.abs(obj.center[0] - obj.startP[0]), 2) + Math.pow(Math.abs(obj.center[1] - obj.startP[1]), 2)) > 500) {
+		bulet.splice(bulet.indexOf(obj), 1);
+	}
+}
+
 function objectMove(obj, course) {
 
 	if(!course) {course = 1;}
@@ -95,9 +122,7 @@ function objectMove(obj, course) {
 }
 
 function objectRotate(obj) {
-
 	objR = [];
-
 	obj.points.forEach(function(item, i, arr) {
 
 		itemR0 = item[0] * Math.cos(obj.angle) - item[1] * Math.sin(obj.angle);
@@ -113,18 +138,10 @@ function objectRotate(obj) {
 
 var drawObject = function(obj, a) {
 
-	if (leftR == true) {baza.angle -= 3 * Math.PI / 180;}
-	if (rightR == true) {baza.angle += 3 * Math.PI / 180;}
-	if (frontM == true) {objectMove(baza);}
-	if (backM == true) {objectMove(baza, -1);}
-
-	if (a == "auto") {angleValue(obj)}
-
 	objP = objectRotate(obj);
 
 	ctx.fillStyle = obj.bg;
 	ctx.beginPath();
-
 
 	objP.forEach(function(item, i, arr) {
 		ctx.lineTo(obj.center[0] + item[0], obj.center[1] + item[1]);
@@ -134,21 +151,22 @@ var drawObject = function(obj, a) {
 	ctx.fill();
 	ctx.stroke();
 	ctx.closePath();
-
 }
 
 
 var draw = function() {
 	return function(){
 		ctx.clearRect(-20, -20, w+20, h+20);
-		drawObject(baza);
-		plan.center = baza.center;
-		drawObject(plan, "auto");
-		if(bulet.shoot.length > 0) {drawShoot(bulet)}
-		// angle+=1;
+		baza.draw();
+		plan.draw();
+		if (bulet.length > 0) {
+			bulet.forEach(function(item, i, arr) {
+				arr[i].draw();
+			});
+		}
+	console.log(bulet.length);
 	}
-
 };
 
 init();
-setInterval(draw(),40);
+setInterval(draw(),35);
